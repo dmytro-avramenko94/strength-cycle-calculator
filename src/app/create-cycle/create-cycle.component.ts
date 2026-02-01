393d02f307a124313858be583b08365346b6a2d2
Dmytro Avramenko <avram2705@gmail.com>
2026-02-01 17:59:40 +0100

    KAN-4 (Create Angular project + basic pages)

---
+++ src/app/create-cycle/create-cycle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-cycle',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-cycle.component.html',
+  styleUrl: './create-cycle.component.css'
+})
+export class CreateCycleComponent {
+
+}
